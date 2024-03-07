7bc49b3a60cfdc2d96e717ce79d89570430bb57d
karini925 <karin.isaev@gmail.com>
2024-03-07 17:27:10 -0500

    package 0.1.1

--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.9.15',
     include_package_data=False, 
 )
